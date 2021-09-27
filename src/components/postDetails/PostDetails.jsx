e5979cadd26fa2caeb91b8913f6347c58dde6b93
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-09-27 23:04:04 +0300

    elimination bug

--- src/components/postDetails/PostDetails.jsx
+++ src/components/postDetails/PostDetails.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import PersonIcon from "@mui/icons-material/Person";
 
 function PostDetails() {
   return (
     <div className="postDetails">
-      <span className="postDetails__author">John Doe</span>
-      <div className="postDetails__categories">
-        <span className="postDetails_category">Technology</span>
-        <span className="postDetails_category">Coding</span>
+      <span className="postDetails__wrapper postDetails__author">
+        <PersonIcon className="postDetails__icon" /> John Doe
+      </span>
+      <div className="postDetails__wrapper postDetails__categories">
+        <FolderOpenIcon className="postDetails__icon" />
+        <span className="postDetails__category">Technology</span>
+        <span className="postDetails__category">Coding</span>
       </div>
-      <time className="postDetails__date" dateTime="">
-        2 hours ago
+      <time className="postDetails__wrapper postDetails__date" dateTime="">
+        <DateRangeIcon className="postDetails__icon" /> 2 hours ago
       </time>
     </div>
   );
